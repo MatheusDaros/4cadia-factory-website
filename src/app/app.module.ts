6ce61fd084063a6e9a1aef70ac5a7d33e321074c
Matheus Pagani <matheusdaros77@hotmail.com>
2020-10-28 15:52:34 -0300

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SurveyComponent } from './survey/survey.component';
+import { ModalModule } from './_modal';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ModalService } from '../_modal';
+import exitIntent from 'exit-intent';
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+	selector: 'app-landing-page',
+	templateUrl: './landing-page.component.html',
+	styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+	constructor(private modalService: ModalService) {}
 
-  constructor() { }
+	ngOnInit(): void {
+    const removeExitIntent = exitIntent({
+      threshold: 50,
+      maxDisplays: 1,
+      eventThrottle: 100,
+      onExitIntent: () => {
+        this.onMouseLeave();
+      }
+    })
+	}
 
-  ngOnInit(): void {
-  }
+	openModal(id: string) {
+		this.modalService.open(id);
+	}
 
+	closeModal(id: string) {
+		this.modalService.close(id);
+	}
+
+	onMouseLeave() {
+		this.openModal('popup-mouseleave');
+	}
 }
